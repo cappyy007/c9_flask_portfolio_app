38729da8007203a6258318d24c0d8561fc3bb864
cappyy007 <71184205+cappyy007@users.noreply.github.com>
2020-09-13 13:27:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name=('Likes to eat pussy')
+	new_name=name + ('Likes to eat pussy')
 	return render_template('index.html', name=new_name)
 
 
