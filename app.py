47c3872726d3ff30c8e822552397aede86a518ae
cappyy007 <71184205+cappyy007@users.noreply.github.com>
2020-09-14 10:44:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -57,6 +57,22 @@ def shopping_list_post():
             return render_template('shopping_list.html', result="\n".join([str(item) for item in shop_list]))
           except ValueError:
             return "Easy now! Let's keep it simple! Just words with a space between them"
+
+@app.route('/add_numbers', methods=['GET','POST'])
+def add_numbers_post():
+	  # --> ['5', '6', '8']
+	  # print(type(request.form['text']))
+	  if request.method == 'GET':
+	  	return render_template('add_numbers.html')
+	  elif request.method == 'POST':
+  	      print(request.form['text'].split())
+  	      total = 0
+  	      try:
+  	      	for str_num in request.form['text'].split():
+  	      		total += int(str_num)
+  	      	return render_template('add_numbers.html', result=str(total))
+  	      except ValueError:
+  	      	return "Easy now! Let's keep it simple! 2 numbers with a space between them please"
           
   	      
 @app.route('/time', methods=['GET','POST'])
